374fb23826048edd18148d845f8168909a67d79f
Robby Sodhi <42954808+Robstersgaming@users.noreply.github.com>
2021-06-14 17:17:27 -0400

    update :(

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,35 @@
 import React from "react";
-
+import Header from "./header";
+import BottomBar from "./bottomBar";
+import { Redirect } from "react-router-dom";
 export default class Dashboard extends React.Component {
+  state = {
+    redirect: null,
+  };
+
+  getCookieValue = (name) =>
+    document.cookie.match("(^|;)\\s*" + name + "\\s*=\\s*([^;]+)")?.pop() || "";
+  componentDidMount() {
+    let session_key = this.getCookieValue("session-key");
+    console.log(session_key);
+    if (!session_key) {
+      this.setState({ redirect: "/Home" });
+    }
+    fetch(""); //send session key to server through /getWatchList
+  }
+
   render() {
-    return <p>Dashboard....</p>;
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />;
+    } else {
+      return (
+        <div>
+          <Header />
+          <div style={{ marginTop: "60vh" }}>
+            <BottomBar />
+          </div>
+        </div>
+      );
+    }
   }
 }
